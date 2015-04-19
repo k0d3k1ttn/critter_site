100efa1bb8dae17e5b1e02f3804d83c49a6683af
Kodee <t.m.waldrop@gmail.com>
2015-04-19 15:32:35 -0400

    Added form and edit

--- cat_shelter/urls.py
+++ cat_shelter/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.current_cats),
     url(r'^kitty/(?P<pk>[0-9]+)/$', views.cat_detail),
+    url(r'^cat/new/$', views.cat_new, name='cat_new'),
+    url(r'^cat/(?P<pk>[0-9]+)/edit/$', views.cat_edit, name='cat_edit'),
 ]
--- cat_shelter/views.py
+++ cat_shelter/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Cat
+from .forms import CatForm
 
 # Create your views here.
 
@@ -10,3 +11,24 @@ def current_cats(request):
 def cat_detail(request, pk):
   cat = get_object_or_404(Cat, pk=pk)
   return render(request, 'cat_shelter/cat_detail.html', {'cat':cat})
+
+def cat_new(request):
+  if request.method == "POST":
+    form = CatForm(request.POST)
+    if form.is_valid():
+      cat = form.save()
+      return redirect('cat_shelter.views.cat_detail', pk=cat.pk)
+  else:
+    form = CatForm()
+  return render(request, 'cat_shelter/cat_edit.html', {'form': form})
+
+def cat_edit(request, pk):
+  cat = get_object_or_404(Cat, pk=pk)
+  if request.method == "POST":
+    form = CatForm(request.POST, instance=cat)
+    if form.is_valid():
+      cat = form.save()
+      return redirect('cat_shelter.views.cat_detail', pk=cat.pk)
+  else:
+    form = CatForm(instance=cat)
+  return render(request, 'cat_shelter/cat_edit.html', {'form': form})
