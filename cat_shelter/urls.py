f7de5e4bcbd73f558a1731d2acaf134bab2a021f
Kodee <t.m.waldrop@gmail.com>
2015-04-19 11:15:45 -0400

    cat shelter is linkified

--- cat_shelter/urls.py
+++ cat_shelter/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.current_cats),
+    url(r'^kitty/(?P<pk>[0-9]+)/$', views.cat_detail),
 ]
--- cat_shelter/views.py
+++ cat_shelter/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Cat
 
 # Create your views here.
@@ -6,3 +6,7 @@ from .models import Cat
 def current_cats(request):
   mycats = Cat.objects.all().order_by('age')
   return render(request, 'cat_shelter/current_cats.html', {'mycats' : mycats})
+
+def cat_detail(request, pk):
+  cat = get_object_or_404(Cat, pk=pk)
+  return render(request, 'cat_shelter/cat_detail.html', {'cat':cat})
